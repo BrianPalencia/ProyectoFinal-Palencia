ec5d3be3c09b0d71f1f1c649606711fdb17555a2
Brian Palencia <briandonaldo@gmail.com>
2023-08-19 14:48:02 -0600

    agregando cambios solicitados por el tutor

--- main.js
+++ main.js
@@ -1,17 +1,60 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const loginForm = document.getElementById("logueo");
+    const registroForm = document.getElementById("registroForm");
+    const loginForm = document.getElementById("loginForm");
+    const registroDiv = document.getElementById("registro");
+    const loginDiv = document.getElementById("login");
+    const iniciaSesionLink = document.getElementById("iniciaSesionLink");
+    const registrateLink = document.getElementById("registrateLink");
+    const loginMensajeError = document.getElementById("loginMensajeError");
+
+    registroForm.addEventListener("submit", function(event) {
+        event.preventDefault();
+
+        const registroUsuario = document.getElementById("registroUsuario").value;
+        const registroContraseña = document.getElementById("registroContraseña").value;
+
+        const users = JSON.parse(localStorage.getItem("users")) || [];
+        users.push({ usuario: registroUsuario, contraseña: registroContraseña });
+        localStorage.setItem("users", JSON.stringify(users));
+
+        registroDiv.style.display = "none";
+        loginDiv.style.display = "block";
+    });
 
     loginForm.addEventListener("submit", function(event) {
         event.preventDefault();
 
-        const usuario = document.getElementById("usuario").value;
-        const contraseña = document.getElementById("contraseña").value;
+        const loginUsuario = document.getElementById("loginUsuario").value;
+        const loginContraseña = document.getElementById("loginContraseña").value;
 
-        localStorage.setItem("usuario", usuario);
-        localStorage.setItem("contraseña", contraseña);
+        const users = JSON.parse(localStorage.getItem("users")) || [];
+        const user = users.find(u => u.usuario === loginUsuario && u.contraseña === loginContraseña);
 
-        window.location.href = "page.html";
+        if (user) {
+            window.location.href = "page.html";
+        } else {
+            loginMensajeError.textContent = "Usuario o contraseña incorrectos. Por favor, intenta nuevamente.";
+            loginMensajeError.style.display = "block";
+        }
     });
+
+    iniciaSesionLink.addEventListener("click", function(event) {
+        event.preventDefault();
+        registroDiv.style.display = "none";
+        loginDiv.style.display = "block";
+    });
+
+    registrateLink.addEventListener("click", function(event) {
+        event.preventDefault();
+        loginDiv.style.display = "none";
+        registroDiv.style.display = "block";
+    });
+
+    const users = JSON.parse(localStorage.getItem("users")) || [];
+    if (users.length > 0) {
+        registroDiv.style.display = "none";
+        loginDiv.style.display = "block";
+    }
 });
 
 document.addEventListener("DOMContentLoaded", function() {
@@ -67,15 +110,38 @@ document.addEventListener("DOMContentLoaded", function() {
     const cotizarboton = document.getElementById("cotizarboton");
     const resultado = document.getElementById("resultado");
 
-    cotizarboton.addEventListener("click", calculateTotalCost);
+    mostrarCotizaciones();
 
-    function calculateTotalCost() {
+    cotizarboton.addEventListener("click", calcularCostoTotal);
+
+    function mostrarCotizaciones() {
+        const cotizaciones = JSON.parse(localStorage.getItem("cotizaciones")) || [];
+        const cotizacionesDiv = document.getElementById("cotizaciones");
+        cotizacionesDiv.innerHTML = "";
+
+        cotizaciones.forEach(cotizacion => {
+            const cotizacionItem = document.createElement("div");
+            cotizacionItem.classList.add("cotizacion-item");
+            cotizacionItem.textContent = cotizacion.cotizacion;
+            cotizacionesDiv.appendChild(cotizacionItem);
+        });
+    }    
+
+    function calcularCostoTotal() {
         const seleccionarPrecio = parseFloat(seleccionarVehiculo.value);
         const diasSeleccionados = parseInt(dias.value, 10);
-
+    
         if (!isNaN(seleccionarPrecio) && !isNaN(diasSeleccionados)) {
-            const totalCost = seleccionarPrecio * diasSeleccionados;
-            const cotizacionText = `La cotización de renta de este vehículo por ${diasSeleccionados} días es de: Q. ${totalCost}`;
+            const nombreVehiculo = seleccionarVehiculo.options[seleccionarVehiculo.selectedIndex].text;
+            const costoTotal = seleccionarPrecio * diasSeleccionados;
+            const cotizacionText = `La cotización de ${nombreVehiculo} por ${diasSeleccionados} días es de: Q. ${costoTotal}`;
+
+            const cotizaciones = JSON.parse(localStorage.getItem("cotizaciones")) || [];
+            cotizaciones.push({ cotizacion: cotizacionText });
+            localStorage.setItem("cotizaciones", JSON.stringify(cotizaciones));
+    
+            mostrarCotizaciones();
+    
             resultado.textContent = cotizacionText;
         } else {
             resultado.textContent = "Por favor, selecciona un vehículo y los días de alquiler.";
